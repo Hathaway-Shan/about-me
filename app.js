4039af8a2aa17f212c5fa7cf071bdaafb9a2b288
Hathaway-Shan <hathaway.shan@gmail.com>
2022-06-01 16:08:05 -0700

    added checkbox event lister

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const tosCheckbox = document.getElementById('tos-checkbox');
 const showAnswerButton = document.getElementById('show-animal-button');
 const animalSection = document.getElementById('animal-reveal');
 // set event listeners
+tosCheckbox.addEventListener('change', () => {
+    showAnswerButton.disabled = !tosCheckbox.ariaChecked;
+});
     // get info from user input
     // use user input to update state
     // update DOM to reflect the new state
