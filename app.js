8ef5aa254568f1158b9a1f39870aae1339282cf3
Hathaway-Shan <hathaway.shan@gmail.com>
2022-06-01 16:02:36 -0700

    changed some css added checkbox button and animal section variable

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // grab DOM elements
-
+const tosCheckbox = document.getElementById('tos-checkbox');
+const showAnswerButton = document.getElementById('show-animal-button');
+const animalSection = document.getElementById('animal-reveal');
 // set event listeners
     // get info from user input
     // use user input to update state
