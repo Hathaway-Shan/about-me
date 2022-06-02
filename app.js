dc824f4aa7221dcad0e902890e69e86750575143
Hathaway-Shan <hathaway.shan@gmail.com>
2022-06-01 17:02:41 -0700

    added new variable and id tags in HTML

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 const tosCheckbox = document.getElementById('tos-checkbox');
 const showAnswerButton = document.getElementById('show-animal-button');
 const animalSection = document.getElementById('animal-reveal');
+
+const nameInput = document.getElementById('this-fluff');
+cont nameDisplay = document.getElementById('name-display');
+const imageSelect = document.getElementById('image-select');
+const imageDisplay = document.getElementById('image-display');
 // set event listeners
 tosCheckbox.addEventListener('change', () => {
     showAnswerButton.disabled = !tosCheckbox.checked;
@@ -9,6 +14,9 @@ tosCheckbox.addEventListener('change', () => {
 showAnswerButton.addEventListener('click', () => {
     animalSection.classList.remove('hidden');
 });
+
+
+
     // get info from user input
     // use user input to update state
     // update DOM to reflect the new state
